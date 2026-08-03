0b894a5258d0522e2e4200c4f891093d407f9164
xoteki <xotekijke@gmail.com>
2026-08-03 10:44:52 +0300

    Initial commit

---
+++ src/main/java/com/example/accommodation/AccommodationApplication.java
@@ -0,0 +1,13 @@
+package com.example.accommodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccommodationApplication.class, args);
+    }
+
+}
